2a2b4e4b7ad51bb4240b3b61a7e4b8b191c9590d
Spencer Baird <spencer.baird36@gmail.com>
2019-01-13 16:30:54 -0500

    Scraper added

--- index.js
+++ index.js
@@ -1,18 +1,53 @@
 const express = require("express");
 const path = require("path");
+const rp = require("request-promise");
+const cheerio = require("cheerio");
 
 const app = express();
 
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, "client/build")));
 
-// An api endpoint that returns a short list of items
-app.get("/api/getList", (req, res) => {
-  var list = ["item1", "item2", "item3"];
-  res.json(list);
-  console.log("Sent list of items");
-});
+app.get("/api/year/:year", (req, res) => {
+  const year = req.params.year;
+  const url = `https://www.pgatour.com/tournaments/sony-open-in-hawaii/past-results/jcr:content/mainParsys/pastresults.selectedYear.${year}.html`;
+  let options = {
+    uri: url,
+    transform: function(body) {
+      return cheerio.load(body);
+    }
+  };
 
+  rp(options).then($ => {
+    let results = [];
+    const table = $(".table-styled").find("tr:not(:first-child)");
+    for (let i = 0; i < table.length; i++) {
+      let player = {};
+      let current = table[i];
+      let name = $(current)
+        .children("td:nth-child(1)")
+        .text()
+        .trim();
+
+      let position = $(current)
+        .children("td:nth-child(2)")
+        .text()
+        .trim();
+
+      let winnings = $(current)
+        .children("td:nth-child(8)")
+        .text()
+        .trim();
+
+      player.name = name;
+      player.position = position;
+      player.winnings =
+        winnings === "" ? 0 : parseFloat(winnings.replace(/\$|,/g, ""));
+      results.push(player);
+    }
+    res.json(results.slice(1));
+  });
+});
 // Handles any requests that don't match the ones above
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
