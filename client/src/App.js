4ee31774212ecde56fe9b97d269001d99aadd0b6
Spencer Baird <spencer.baird36@gmail.com>
2019-01-13 22:12:23 -0500

    Able to fetch backend data

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,58 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from "react";
+import axios from "axios";
+import "./App.css";
+import { Dropdown, Segment } from "semantic-ui-react";
 
 class App extends Component {
+  state = {
+    tournaments: [],
+    currentTournamet: null,
+    value: null,
+    historicalResults: []
+  };
+
+  componentWillMount() {
+    axios.get("/api/schedule").then(response => {
+      const tournamentOptions = response.data.tournaments.map(elem => {
+        return {
+          value: elem.htmlString,
+          text: elem.name
+        };
+      });
+
+      this.setState({
+        tournaments: [...this.state.tournaments, ...tournamentOptions],
+        currentTournamet: response.data.currentTournamet
+      });
+    });
+  }
+
+  updateTournament = (e, { value }) => {
+    const formattedString = value.slice(13);
+    const finalFormatedString = formattedString.slice(0, -5);
+    this.setState({ value: finalFormatedString }, () => {
+      axios.get(`/api/${this.state.value}/2018`).then(response => {
+        this.setState({
+          historicalResults: response.data
+        });
+      });
+    });
+  };
+
   render() {
+    console.log(this.state);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Fragment>
+        <Segment>
+          <Dropdown
+            placeholder="Select Tournament"
+            fluid
+            selection
+            options={this.state.tournaments}
+            onChange={this.updateTournament}
+          />
+        </Segment>
+      </Fragment>
     );
   }
 }
